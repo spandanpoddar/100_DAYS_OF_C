37a4fb9c91b53427b368ee1e4d08fea68a0ce569
spandanpoddar <spandanedu26@gmail.com>
2026-08-17 12:54:21 +0530

    Add files via upload

---
+++ arec.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int l,b;
+  printf("enter integer length value of l:");
+  scanf("%d",&l);
+  
+  printf("enter integer breadth value of b:");
+  scanf("%d",&b);
+  
+  printf("the area of rectangle is : %d * %d is %d\n", l,b,l*b);
+}
---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("enter integer value of a:");
+ scanf("%d",&a);
+ 
+ printf("enter integer value of b:");
+ scanf("%d",&b);
+ 
+ printf("the sum of: %d + %d is %d\n", a,b,a+b);
+}
